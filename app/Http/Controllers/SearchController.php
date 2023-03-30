fff82efd6e64a767cd49d4128fb1a5e58cbaf287
Hero Franesa <hiro.franesa@gmail.com>
2023-03-30 17:09:23 +0700

    Created Function Search Autocomplete With Ajax

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\View\View;
+use Illuminate\Http\JsonResponse;
 
 class SearchController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view("searchDemo");
+    }
+
+    public function autocomplete(Request $request): JsonResponse
+    {
+        $data = [];
+
+        if ($request->filled('q')) {
+            $data = User::select("name", "id")
+                ->where("name", "LIKE", "%" . $request->get("q") . "%")
+                ->get();
+        }
+        return response()->json($data);
+    }
 }
